170ab1ee34fa901f7d3cf7334f8a8a1cb2abfe01
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2022-01-22 18:30:34 +0900

    dekita

--- test/outside-src/stencil.config.ts
+++ test/outside-src/stencil.config.ts
@@ -1,11 +1,22 @@
-import { Config } from '../../internal';
+import { CompilerCtx, Config } from '../../internal';
+import path from 'node:path';
 
 export const config: Config = {
   srcDir: 'src/app1',
+  buildDocs: true,
   outputTargets: [
     {
       type: 'www',
       serviceWorker: null,
+    },
+    {
+      type: 'custom',
+      name: 'watchAdditional',
+      async generator(stencilConfig, compilerCtx: CompilerCtx) {
+        if (!stencilConfig.watch) return;
+        compilerCtx.addWatchFile(path.resolve('.', 'src/shared/components/shared-panel.tsx'));
+        compilerCtx.addWatchFile(path.resolve('.', 'src/shared/components/shared-panel.css'));
+      },
     },
   ],
   devServer: {
